b1e440a443308405bda9d5e86ce2850f2cb45d90
Ullas-browny <svenshine339@outlook.com>
2023-03-23 22:42:02 +0530

    made changes

--- src/GQTCodes/program1.java
+++ src/GQTCodes/program1.java
@@ -7,7 +7,7 @@ public class program1 {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
-		System.out.println("Hello World!");
+		
 	}
 
 }
